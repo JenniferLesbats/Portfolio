c1601d898327afedb4572fa55581311c45a58486
Jennifer Lesbats <103788214+JenniferLesbats@users.noreply.github.com>
2023-01-13 22:11:54 +0100

    Add files via upload

--- jsperso.js
+++ jsperso.js
@@ -1,6 +1,6 @@
 
 
-const agrandissementImagePodcast = document.querySelector("#affichePodcast")
+/*const agrandissementImagePodcast = document.querySelector("#affichePodcast")
 agrandissementImagePodcast.addEventListener('click', imagePodcastFullScreen)
 
 function imagePodcastFullScreen (){
@@ -12,4 +12,21 @@ agrandissementInfographie.addEventListener('click', infographieFullScreen)
 
 function infographieFullScreen (){
     agrandissementInfographie.requestFullscreen()
+}*/
+
+const agrandissementImagePodcast = document.querySelector("#affichePodcast")
+var requestFullScreen = agrandissementImagePodcast.requestFullscreen || agrandissementImagePodcast.msRequestFullscreen || agrandissementImagePodcast.mozRequestFullScreen || agrandissementImagePodcast.webkitRequestFullscreen;
+agrandissementImagePodcast.addEventListener('click', imagePodcastFullScreen)
+
+function imagePodcastFullScreen (){
+    requestFullScreen.call(agrandissementImagePodcast)
+}
+
+
+const agrandissementInfographie = document.querySelector("#infographie")
+var requestFullScreen2 = agrandissementInfographie.requestFullscreen || agrandissementInfographie.msRequestFullscreen || agrandissementInfographie.mozRequestFullScreen || agrandissementInfographie.webkitRequestFullscreen;
+agrandissementInfographie.addEventListener('click', infographieFullScreen)
+
+function infographieFullScreen (){
+    requestFullScreen2.call(agrandissementInfographie)
 }
